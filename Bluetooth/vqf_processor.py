bde1c14ba692cbaf0135417916303d0f2db23909
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-03-28 12:30:28 -0400

    removed VQF, added sagemotion calculations and fixed parsing

--- Bluetooth/vqf_processor.py
+++ Bluetooth/vqf_processor.py
@@ -1,28 +1,55 @@
 import asyncio
 import struct
-from vqf import VQF
+import math
+
+from FPA_algorithm import FPA
+from gaitphase import GaitPhase
 
 from bluetooth import find_device, BLEConnection
+ 
+IS_RIGHT_FOOT = True  
 
-SAMPLE_RATE = 100.0 
 
 def parse_payload(payload: bytes):
-    # need to implement
+    if len(payload) < 24:
+        return None
+    ax, ay, az, gx, gy, gz = struct.unpack_from('<6f', payload)
+    acc = [ax, ay, az]
+    gyr = [gx, gy, gz]
+    return gyr, acc
 
-async def vqf_consumer(packet_queue: asyncio.Queue, vqf: VQF):
+
+async def fpa_consumer(packet_queue: asyncio.Queue, gp: GaitPhase, fpa: FPA):
     while True:
-        seq, payload, rate = await packet_queue.get()
+        payload, rate = await packet_queue.get()
 
         parsed = parse_payload(payload)
         if parsed is None:
-            print(f"seq={seq} could not parse payload ({len(payload)} bytes)")
+            print(f"could not parse payload")
             continue
 
         gyr, acc = parsed
-        vqf.update(gyr, acc)
 
-        quat = vqf.getQuat6D()   # [w, x, y, z]
-        print(f"seq={seq}  rate={rate:.1f} Hz  quat={quat}")
+        # Expected payload format from MCU:
+        #   6 little-endian 32-bit floats = 24 bytes total
+        #   [ax, ay, az, gx, gy, gz]
+        #   ax/ay/az: accelerometer in m/s²
+        #   gx/gy/gz: gyroscope in rad/s
+
+        # convert gyro from rad/s to deg/s for FPA algorithm
+        sensor_data = {
+            "AccelX": acc[0], "AccelY": acc[1], "AccelZ": acc[2],
+            "GyroX": math.degrees(gyr[0]),
+            "GyroY": math.degrees(gyr[1]),
+            "GyroZ": math.degrees(gyr[2]),
+        }
+
+        gp.update_gaitphase(sensor_data)
+        fpa.update_FPA(sensor_data, gp.gaitphase_old, gp.gaitphase)
+
+        #RIGHT NOW WE PRINT. BUT LATER, WE RUN SCRIPT THAT SENDS VIB FEEDBACK COMMANDS TO SHANK COMPONENT
+        if gp.in_feedback_window:
+            print(f"Step {gp.step_count}: FPA = {fpa.FPA_this_step:.1f} deg  rate={rate:.1f} Hz")
 
 
 async def main():
@@ -33,13 +60,14 @@ async def main():
 
     print("Connected!")
     packet_queue = asyncio.Queue()
-    vqf = VQF(1.0 / SAMPLE_RATE)
+    gp  = GaitPhase(datarate=180)
+    fpa = FPA(is_right_foot=IS_RIGHT_FOOT, datarate=180)
 
     conn = BLEConnection(packet_queue=packet_queue)
 
     await asyncio.gather(
         conn.connect_and_read(address),
-        vqf_consumer(packet_queue, vqf),
+        fpa_consumer(packet_queue, gp, fpa),
     )
 
 if __name__ == "__main__":
